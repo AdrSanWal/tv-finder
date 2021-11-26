31760d644e847ea202c257037983b91dc2c03b26
AdrSanWal <wallace581@hotmail.com>
2021-11-26 22:30:18 +0100

    delete series function

--- tvfinder/tv/urls.py
+++ tvfinder/tv/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('f/', views.films, name='films'),
-    path('s/', views.series, name='series'),
+    path('<filter_type>/', views.films, name='films'),
     path('<filter_type>/<int:filter_id>/', views.filtered_film, name='filtered_film'),
 ]
--- tvfinder/tv/views.py
+++ tvfinder/tv/views.py
@@ -3,25 +3,15 @@ from django.shortcuts import render
 from .models import Tv, Director, Gender
 
 
-def filter_films(request, film_type):
+def films(request, filter_type):
     finder = request.GET.get('finder')
     if finder != '' and finder is not None:
-        filtered = Tv.objects.filter(tv_type=film_type, title__icontains=finder)
-        return {'all_tv': filtered}
-
-
-def films(request):
-    if (context := filter_films(request, 'f')):
-        return render(request, 'tv/tv.html', context)
-    films_titles = Tv.objects.filter(tv_type='f')
-    return render(request, 'tv/tv.html', {'all_tv': films_titles})
-
-
-def series(request):
-    if (context := filter_films(request, 's')):
-        return render(request, 'tv/tv.html', context)
-    series_titles = Tv.objects.filter(tv_type='s')
-    return render(request, 'tv/tv.html', {'all_tv': series_titles})
+        filtered = Tv.objects.filter(tv_type=filter_type, title__icontains=finder)
+        context = {'all_tv': filtered}
+    else:
+        all_tv = Tv.objects.filter(tv_type=filter_type)
+        context = {'all_tv': all_tv}
+    return render(request, 'tv/tv.html', context)
 
 
 def filtered_film(request, filter_type, filter_id):
