7ac5fa72be536f20d34adfe6601bcec04e98f358
AdrSanWal <wallace581@hotmail.com>
2021-11-26 18:26:38 +0100

    sidebar position better

--- tvfinder/tv/static/tv/js/scripts.js
+++ tvfinder/tv/static/tv/js/scripts.js
@@ -1,42 +1,6 @@
-function filterTv(e, element) {
-    let keynum;
-    if(window.event) { // IE                  
-        keynum = e.keyCode;
-      } else if(e.which){ // Netscape/Firefox/Opera                 
-        keynum = e.which;
-      }
-    alert(element.value + String.fromCharCode(keynum))
-}
-
-$("#menu-toggle").click(function(e) {
-    alert('carga')
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
- });
- $("#menu-toggle-2").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled-2");
-    $('#menu ul').hide();
- });
- 
- function initMenu() {
-    $('#menu ul').hide();
-    $('#menu ul').children('.current').parent().show();
-    //$('#menu ul:first').show();
-    $('#menu li a').click(
-       function() {
-          var checkElement = $(this).next();
-          if ((checkElement.is('ul')) && (checkElement.is(':visible'))) {
-             return false;
-          }
-          if ((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
-             $('#menu ul:visible').slideUp('normal');
-             checkElement.slideDown('normal');
-             return false;
-          }
-       }
-    );
- }
- $(document).ready(function() {
-    initMenu();
- });
+ $(document).ready(function(){
+    $("#menu-toggle").click(function(e){
+      e.preventDefault();
+      $("#wrapper").toggleClass("menuDisplayed");
+    });
+  });
